7ac9611ff12770a8e259c2602d00c04a2c2d2108
Alex Haydock <alex@alexhaydock.co.uk>
2019-08-10 12:22:52 +0100

    Streamline Docker build

--- docker/docker-test-site.sh
+++ docker/docker-test-site.sh
@@ -6,7 +6,7 @@ docker build -t alexhaydock .
 
 docker run --rm -it \
   --name "alexhaydock" \
-  -v "/home/a/gitlab/alexhaydock.co.uk:/opt/www" \
+  -v "$(pwd)/../:/opt/www" \
   -p "127.0.0.1:4000:4000/tcp" \
   alexhaydock \
-    bundle exec jekyll serve --incremental -H 0.0.0.0
+    bundle exec jekyll serve -H 0.0.0.0
