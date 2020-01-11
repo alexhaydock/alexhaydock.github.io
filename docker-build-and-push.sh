c05b4c73c9041ec71d441968f504d39495bff34d
Alex Haydock <alex@alexhaydock.co.uk>
2020-01-11 18:33:10 +0000

    64-bit ARM preparedness

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if [ "$EUID" -eq 0 ]; then echo "Please do not run as root. Please add yourself to the 'docker' group."; exit; fi
+if [ "$EUID" -eq 0 ]; then echo "Please do not run as root. Please add yourself to the 'docker' group or use Podman."; exit; fi
 
 if [ "$(uname -m | grep 'x86_64' | wc -l)" -gt 0 ]; then
     echo "This looks like an x86_64 system. Building for that platform."
@@ -10,7 +10,12 @@ elif [ "$(uname -m | grep 'armv7l' | wc -l)" -gt 0 ]; then
     echo "This looks like an armv7l system. Building for that platform."
     docker build --no-cache -t registry.gitlab.com/alexhaydock/alexhaydock.co.uk:armv7l .
     docker push registry.gitlab.com/alexhaydock/alexhaydock.co.uk:armv7l
+elif [ "$(uname -m | grep 'aarch64' | wc -l)" -gt 0 ]; then
+    echo "This looks like an aarch64 system. Building for that platform."
+    docker build --no-cache -t registry.gitlab.com/alexhaydock/alexhaydock.co.uk:aarch64 .
+    docker push registry.gitlab.com/alexhaydock/alexhaydock.co.uk:aarch64
 else
     echo "Sorry, I don't understand this processor architecture."
     echo "Please build manually."
 fi
+
--- docker-test-site.sh
+++ docker-test-site.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -xe
 
+docker build -t alehaydock .
+
 docker run --rm -it \
   --name "alexhaydock" \
   -v $(pwd)/:/opt/www/ \
   -p "127.0.0.1:4000:4000/tcp" \
   --workdir /opt/www \
-  registry.gitlab.com/alexhaydock/dockerfiles:jekyll \
+  alexhaydock \
     bundle exec jekyll serve -H 0.0.0.0
